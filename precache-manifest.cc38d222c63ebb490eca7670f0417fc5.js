9ce3ee834aaa8afe31265e6bd18a8c921a1b00e8
ronyshchenko <roma.onyshchenko@gmail.com>
2021-03-25 18:56:42 +0200

    Updates

---
+++ precache-manifest.cc38d222c63ebb490eca7670f0417fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcab1c0d796c94ecaf54245f497cd9b8",
+    "url": "/react-people-table/index.html"
+  },
+  {
+    "revision": "1a7b44d02c104addc7d7",
+    "url": "/react-people-table/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "77292d8a0cb365d532f8",
+    "url": "/react-people-table/static/css/main.cedc4347.chunk.css"
+  },
+  {
+    "revision": "1a7b44d02c104addc7d7",
+    "url": "/react-people-table/static/js/2.c219f1e2.chunk.js"
+  },
+  {
+    "revision": "77292d8a0cb365d532f8",
+    "url": "/react-people-table/static/js/main.2bb2f5dc.chunk.js"
+  },
+  {
+    "revision": "f5ae9fbe6e8da64c4b28",
+    "url": "/react-people-table/static/js/runtime~main.4d7c018d.js"
+  }
+]);
